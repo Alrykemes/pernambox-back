eeb1ec9d569f408c0b81f7f31558865d19c11914
Alrykemess <alrykemes.cavalcanti@anota.ai>
2025-09-15 11:11:33 -0300

    feat: add scatch project

---
+++ src/main/java/com/dev/pernambucofre/PernambucofreApplication.java
@@ -0,0 +1,13 @@
+package com.dev.pernambucofre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PernambucofreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PernambucofreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/pernambucofre/PernambucofreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.pernambucofre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PernambucofreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
