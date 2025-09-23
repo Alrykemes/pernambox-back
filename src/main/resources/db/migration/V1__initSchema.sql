758ffb2e9a03f4a13ceaaca73d7134e67717aa1d
Alrykemess <alrykemes.cavalcanti@anota.ai>
2025-09-23 08:43:09 -0300

    feat: add new migration

--- src/main/resources/db/migration/V1__initSchema.sql
+++ src/main/resources/db/migration/V1__initSchema.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE polo
 (
-    id        SERIAL PRIMARY KEY,
+    id        UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     nome      VARCHAR(150) NOT NULL,
     numero    VARCHAR(10)  NOT NULL,
     rua       VARCHAR(150) NOT NULL,
@@ -15,12 +15,12 @@ CREATE TABLE polo
 CREATE TABLE usuario
 (
     id       UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    nome     VARCHAR(150) NOT NULL,
-    cpf      VARCHAR(11)  NOT NULL UNIQUE,
-    email    VARCHAR(150) NOT NULL UNIQUE,
-    telefone VARCHAR(20),
+    nome     VARCHAR(255) NOT NULL,
+    cpf      VARCHAR(14)  NOT NULL UNIQUE,
+    email    VARCHAR(255) NOT NULL UNIQUE,
+    telefone VARCHAR(14),
     senha    VARCHAR(255) NOT NULL,
-    id_polo  INT          NOT NULL,
-    CONSTRAINT fk_usuario_polo FOREIGN KEY (id_polo)
+    polo_id  UUID          NOT NULL,
+    CONSTRAINT fk_usuario_polo FOREIGN KEY (polo_id)
         REFERENCES polo (id) ON DELETE CASCADE
 );
