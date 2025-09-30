fbfdc2d1044f00dc40a142df3f3cba1eeed94fe8
eltonluiz178 <eltonluiz533@gmail.com>
2025-09-30 09:36:02 -0300

    feat: Adicionar novas tabelas as migrations para sistema

--- src/main/resources/db/migration/V3__alterTableUsuario.sql
+++
@@ -1,5 +0,0 @@
-CREATE TYPE tipo_permissao AS ENUM ('ADM_GERAL','ADM_POLO','FUNCIONARIO');
-
-ALTER TABLE usuario ADD COLUMN permissao tipo_permissao NOT NULL DEFAULT 'FUNCIONARIO';
-
-UPDATE usuario SET permissao='ADM_GERAL';
