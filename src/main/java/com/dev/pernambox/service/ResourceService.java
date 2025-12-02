3f94385bfa7b92616930a47d24498056468dc435
Elton Luiz <eltonluiz533@gmail.com>
2025-12-02 10:43:43 -0300

    feat: Cria base metodo pra salvar recurso

--- src/main/java/com/dev/pernambox/service/ResourceService.java
+++ src/main/java/com/dev/pernambox/service/ResourceService.java
@@ -1,6 +1,12 @@
 package com.dev.pernambox.service;
 
+import com.dev.pernambox.domain.resource.Resource;
+import com.dev.pernambox.domain.resource.dtos.ResourceRequestDto;
+import com.dev.pernambox.domain.unit.Unit;
+import com.dev.pernambox.exceptions.NotFoundException;
 import com.dev.pernambox.repositories.ResourceRepository;
+import com.dev.pernambox.repositories.UnitRepository;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +14,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class ResourceService {
     private final ResourceRepository resourceRepository;
+    private final UnitRepository unitRepository;
+
+    @Transactional
+    public void saveResource(ResourceRequestDto resourceDto) {
+        Resource resource = new Resource(resourceDto);
+
+        Unit unit = unitRepository.findById(resource.getUnit_id().getId()).orElseThrow(() -> {
+            throw new NotFoundException("Unit not found");
+        });
+
+
+    }
 }
